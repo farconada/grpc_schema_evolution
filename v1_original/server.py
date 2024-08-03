e87773f7807167e77c22e1c0da1a40594979eb6b
farconada <fernando.arconada@gmail.com>
2024-08-03 09:55:24 +0200

    first commit

---
+++ v1_original/server.py
@@ -0,0 +1,24 @@
+import grpc
+from concurrent import futures
+import user_service_pb2
+import user_service_pb2_grpc
+
+class UserServicer(user_service_pb2_grpc.UserServiceServicer):
+    def GetUser(self, request, context):
+        user_id = request.user_id
+        # Simulamos obtener datos de usuario
+        return user_service_pb2.UserResponse(
+            user_id=user_id,
+            name=f"Usuario {user_id}",
+            email=f"usuario{user_id}@ejemplo.com"
+        )
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    user_service_pb2_grpc.add_UserServiceServicer_to_server(UserServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
---
+++ v2_eliminar_campo/cliente_original/client.py
@@ -0,0 +1,14 @@
+import grpc
+import user_service_pb2
+import user_service_pb2_grpc
+from time import sleep
+
+def run():
+    sleep(5)
+    with grpc.insecure_channel('server:50051') as channel:
+        stub = user_service_pb2_grpc.UserServiceStub(channel)
+        response = stub.GetUser(user_service_pb2.UserRequest(user_id=1))
+        print(f"Usuario recibido: ID={response.user_id}, Nombre={response.name}, Email={response.email}")
+
+if __name__ == '__main__':
+    run()
